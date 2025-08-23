f8ac1f3f7457f86c85983b829e36564301c092fe
Hyowon Ahn <hyowon.ahn@student.mancehster.ac.uk>
2025-08-23 16:23:02 +0900

    feat(mcq): create MCQ set with JWT

--- src/main/java/com/andy/recallify/mcq/McqSetController.java
+++ src/main/java/com/andy/recallify/mcq/McqSetController.java
@@ -1,21 +1,38 @@
 package com.andy.recallify.mcq;
 
+import com.andy.recallify.security.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping(path="api/mcq")
+@RequestMapping(path="api/mcqSet")
 public class McqSetController {
 
     private final McqSetService mcqSetService;
 
+    private final JwtUtil jwtUtil;
+
     @Autowired
-    public McqSetController(McqSetService mcqSetService) {
+    public McqSetController(McqSetService mcqSetService, JwtUtil jwtUtil) {
         this.mcqSetService = mcqSetService;
+        this.jwtUtil = jwtUtil;
     }
 
+    @PostMapping("/create")
+    public ResponseEntity<?> createMcqSet(
+            @RequestBody McqSet mcqSet,
+            @RequestHeader("Authorization") String authHeader
+    ) {
+        try {
+            String token = authHeader.replace("Bearer ", "");
+            String email = jwtUtil.extractEmail(token);
+
+            mcqSetService.createSet(mcqSet, email);
+            return ResponseEntity.ok("Set created successfully");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Error: " + e.getMessage());
+        }
+    }
 }
--- src/main/java/com/andy/recallify/mcq/McqSetService.java
+++ src/main/java/com/andy/recallify/mcq/McqSetService.java
@@ -1,5 +1,7 @@
 package com.andy.recallify.mcq;
 
+import com.andy.recallify.user.User;
+import com.andy.recallify.user.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -7,12 +9,19 @@ import org.springframework.stereotype.Service;
 public class McqSetService {
 
     private final McqSetRepository mcqSetRepository;
+    private final UserRepository userRepository;
 
     @Autowired
-    public McqSetService(McqSetRepository mcqSetRepository) {
+    public McqSetService(McqSetRepository mcqSetRepository, UserRepository userRepository) {
         this.mcqSetRepository = mcqSetRepository;
+        this.userRepository = userRepository;
     }
 
-
+    public void createSet(McqSet mcqSet, String email) {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new IllegalArgumentException("User not found"));
+        mcqSet.setUser(user);
+        mcqSetRepository.save(mcqSet);
+    }
 
 }
