eb5a1ad1a533c638c2436a81a6de468c5d95fbba
Hyowon Ahn <hyowon.ahn@student.mancehster.ac.uk>
2025-09-10 19:23:24 +0900

    changed DB from AWS to Supabase

--- src/main/java/com/andy/recallify/user/service/Mailer.java
+++ src/main/java/com/andy/recallify/user/service/Mailer.java
@@ -1,14 +1,19 @@
 package com.andy.recallify.user.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Service
-@RequiredArgsConstructor
 public class Mailer {
     private final JavaMailSender mailSender;
+
+    @Autowired
+    public Mailer(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }
 
     public void send(String to, String subject, String text) {
         var msg = new SimpleMailMessage();
