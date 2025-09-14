c9d65b03ec12edae2df4c668e415c3c8a79da676
Hyowon Ahn <hyowon.ahn@student.mancehster.ac.uk>
2025-09-14 19:30:39 +0900

    fix(email): store email info in .env

--- src/main/java/com/andy/recallify/user/MailerConfig.java
+++ src/main/java/com/andy/recallify/user/MailerConfig.java
@@ -1,5 +1,6 @@
 package com.andy.recallify.user;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -10,19 +11,18 @@ import java.util.Properties;
 @Configuration
 public class MailerConfig {
     @Bean
-    public JavaMailSender mailSender() {
-        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-        mailSender.setHost("smtp.gmail.com");
-        mailSender.setPort(587);
-        mailSender.setUsername("andy.hw.ahn@gmail.com"); // must create a new email
-        mailSender.setPassword("zhprqdzpsulsvodh");      // move this to somewhere private
-
-        Properties p = mailSender.getJavaMailProperties();
-        p.put("mail.transport.protocol", "smtp");          // NOT "smtps"
-        p.put("mail.smtp.auth", "true");
-        p.put("mail.smtp.starttls.enable", "true");
-        p.put("mail.smtp.starttls.required", "true");
-        p.put("mail.debug", "false");                      // set true to debug
-        return mailSender;
+    public JavaMailSender mailSender(
+            @Value("${SPRING_MAIL_HOST}") String host,
+            @Value("${SPRING_MAIL_PORT}") int port,
+            @Value("${SPRING_MAIL_USERNAME}") String user,
+            @Value("${SPRING_MAIL_PASSWORD}") String pass
+    ) {
+        var ms = new JavaMailSenderImpl();
+        ms.setHost(host); ms.setPort(port);
+        ms.setUsername(user); ms.setPassword(pass);
+        var p = ms.getJavaMailProperties();
+        p.put("mail.smtp.auth","true");
+        p.put("mail.smtp.starttls.enable","true");
+        return ms;
     }
 }
