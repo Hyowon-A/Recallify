3285c269d7772b4fb2c5bc92bfc897cfc089a8f4
Hyowon Ahn <hyowon.ahn@student.mancehster.ac.uk>
2025-09-15 16:01:22 +0900

    fix(security): add CORS

--- src/main/java/com/andy/recallify/security/SecurityConfig.java
+++ src/main/java/com/andy/recallify/security/SecurityConfig.java
@@ -32,6 +32,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
+                .cors(Customizer.withDefaults())
                 .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/user/**", "/api/flashcard/**", "/api/mcq/**", "/api/mcqSet/**", "/api/upload/**" ).permitAll()
